96e814924d3b2fcca11afdb9682efe8e9a0f56c3
Barret J. Nobel <bear.nobel@gmail.com>
2015-06-05 15:05:51 -0400

    setup 4 storyboard VC's setup segues to each
    set cell reuseID

---
+++ TaskList/AddTaskViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddTaskViewController.h
+//  TaskList
+//
+//  Created by OSX on 6/5/15.
+//  Copyright (c) 2015 LebonTech solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddTaskViewController : UIViewController
+
+@end
--- TaskList/TableViewController.h
+++ TaskList/TableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  TableViewController.h
 //  TaskList
 //
 //  Created by OSX on 6/5/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface TableViewController : UITableViewController
 
 @end
-
